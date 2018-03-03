a9af3b0ab7ada5935fd09f75efaf3743422f0cdd
robthewebdev <robthewebdev@gmail.com>
2018-03-03 15:33:05 -0600

    added avg and median indicators to charts

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-import {Sparklines, SparklinesLine} from 'react-sparklines';
+import {Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
 
+function average (data) {
+  const sum = data.reduce((a, b) => a + b, 0);
+  const total = data.length;
+  const avg = sum / total;
+
+  return Math.round(avg);
+}
 
 export default (props) => {
       return (
         <div>
           <Sparklines height={120} width={180} data={props.data}>
             <SparklinesLine color={props.color}/>
+            <SparklinesReferenceLine type="avg" />
           </Sparklines>
+          <div>{average(props.data)}</div>
         </div>
       );
-  };
+};
